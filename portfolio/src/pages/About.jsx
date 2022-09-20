34407bfc97d19ef11f5e86cd23ed80aa18fff5e3
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-20 16:48:31 -0400

    routes working

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from "react"
  const About = () => {
     return (
         <div className="about">
-            
+            <p>About page</p>
         </div>
     )
  }
---
+++ portfolio/src/pages/Projects.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+ const Projects = () => {
+    return (
+        <div className="projects">
+            <p>Projects</p>
+        </div>
+    )
+ }
+ export default Projects
