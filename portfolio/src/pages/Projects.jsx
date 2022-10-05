cae8780f296f703e806cb92e9f2b8ab43502dca6
Jackson McHugh <jackson.mchugh@gmail.com>
2022-10-05 02:09:06 -0400

    updating css and projects page

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
  const Projects = () => {
     return (
+        <section className="page">
         <div className="projects">
             <h1>My Projects!</h1>
          <div className="project_list">
@@ -15,6 +16,7 @@ import React from "react"
              <div className="project_list">
                 <h2>Top Cities</h2>
                 <img className="pics" src="https://i.imgur.com/x6sguBp.png"alt='pics' />
+                <p></p>
              <a target='blank' rel='noreferrer' href="https://github.com/Jacksonmchugh/topcities">to Github repo</a>
              </div>
              <div className="project_list">
@@ -25,8 +27,17 @@ import React from "react"
              <p></p>
              <a target='blank' rel='noreferrer' href="https://jacksonmchugh.github.io/Blackjack/"> If not just go check out the Github repo</a>
              </div>
+             <div className="project_list">
+                <h2>Likes!</h2>
+                <img className="pics" src="https://i.imgur.com/8Eox84R.mp4" alt='pics'/>
+                <p>This was the first project of my dev career. I had to create a game using an API. I went with a simple game of BlackJack. Feel free to try my game out.</p>
+             <a target='blank' rel='noreferrer' href="https://jacksonmchugh.github.io/Blackjack/">Wanna play a game of BlackJack?</a>
+             <p></p>
+             <a target='blank' rel='noreferrer' href="https://jacksonmchugh.github.io/Blackjack/"> If not just go check out the Github repo</a>
+             </div>
          
     </div>
+    </section>
     )
  }
  export default Projects
